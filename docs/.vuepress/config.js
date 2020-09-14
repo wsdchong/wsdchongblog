3e812afb611d9543156c00863d082432d8adc719
wsdchong <wsdchong@outlook.com>
2020-09-14 20:25:06 +0800

    热

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -173,11 +173,11 @@ module.exports = {
       {
         choosen: 'gitalk',
         options: {
-          clientID: 'a6e1355287947096b88b',
-          clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-          repo: 'vuepress-theme-vdoing', // GitHub 仓库
-          owner: 'xugaoyi', // GitHub仓库所有者
-          admin: ['xugaoyi'], // 对仓库有写权限的人
+          clientID: 'fd08010c66dd458b5209',
+          clientSecret: '6321c61ca69b44f5013ff1c82be8e5faa53f9003',
+          repo: 'wsdchongblog', // GitHub 仓库
+          owner: 'wsdchong', // GitHub仓库所有者
+          admin: ['wsdchong'], // 对仓库有写权限的人
           // distractionFreeMode: true,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
           id: "<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>", //  页面的唯一标识,长度不能超过50
